dfc5a1102ba09edb5c58d1de1978431d471c9805
KARTIK5799 <thekartiksapkal@gmail.com>
2023-12-29 02:35:23 +0530

    accordian

--- src/components/Accordians/Accordian.jsx
+++ src/components/Accordians/Accordian.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react'
 
 const Accordian = ({title,content}) => {
-    const [isOpen ,setIsOpen]=useState(false)
+    const [isOpen ,setIsOpen]=useState(false);
+
+    const handleToggle=()=>{
+        setIsOpen(!isOpen)
+    }
   return (
     <div>
-      
+      <h4 onClick={handleToggle}>{title}</h4>
     </div>
   )
 }
