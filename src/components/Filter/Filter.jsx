aa519df93247100d0ebe774b79e2e253f489f7a1
KARTIK5799 <thekartiksapkal@gmail.com>
2023-12-29 02:04:59 +0530

    filter css

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -20,28 +20,29 @@ setIsBrandsVisible(!isBrandsVisible)
    }
   return (
     <div className={styles.filterContainer}>
-      <h2>Search Results</h2>
+      <h1>Search Results</h1>
       <div>
-        <h5 className={styles.filterHeader} onClick={handleClickBrand}>Brand <img src={downArrow} alt="Brand" /></h5>
-        {isBrandsVisible?(<div>
+        <h4 className={styles.filterHeader} onClick={handleClickBrand}>Brand <img src={downArrow} alt="Brand" /></h4>
+        {isBrandsVisible?(<div className={styles.filterOption}>
            <label> <input type="checkbox" name="Mango" id="" />Mango</label>
            <label> <input type="checkbox" name="H&M" id="" />H&M</label>
            <label> <input type="checkbox" name="Adidas" id="" />Adidas</label>
            <label> <input type="checkbox" name="Allen Solly" id="" />Allen Solly</label>
         </div>):null}
       </div>
+      <hr/>
       <div>
-        <h5 onClick={handlePriceRange}>Price Range<img src={downArrow} alt="Price Range" /></h5>
-        {isPriceVisible?(<div>
+        <h4 className={styles.filterHeader} onClick={handlePriceRange}>Price Range<img src={downArrow} alt="Price Range" /></h4>
+        {isPriceVisible?(<div className={styles.filterOption}>
             <label> <input type="checkbox" name="under500" id="" />Under 500</label>
             <label> <input type="checkbox" name="1000to3000" id="" />1000 To 3000</label>
            
         </div>):null}
       </div>
-
+      <hr/>
       <div>
-        <h5 onClick={handleRating}>Rating<img src={downArrow} alt="Rating" /></h5>
-        {isRatingVisible?(<div>
+        <h4 className={styles.filterHeader} onClick={handleRating}>Rating<img src={downArrow} alt="Rating" /></h4>
+        {isRatingVisible?(<div className={styles.filterOption}>
             <label> <input type="checkbox" name="5star" id="" />Under 500</label>
             <label> <input type="checkbox" name="4star" id="" />1000 To 3000</label>
             <label> <input type="checkbox" name="3star" id="" />Under 500</label>
