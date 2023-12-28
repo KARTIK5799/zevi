ade13e513c8dcf525aab1a3f132d8f7764e22ec6
KARTIK5799 <thekartiksapkal@gmail.com>
2023-12-29 02:21:27 +0530

    filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,58 +1,129 @@
-import React, { useState } from 'react'
-import downArrow from '../../assets/downArrow.png'
-import styles from './Filter.module.css'
+import React, { useState } from "react";
+import downArrow from "../../assets/downArrow.png";
+import styles from "./Filter.module.css";
 
 const Filter = () => {
-    const [isBrandsVisible,setIsBrandsVisible]=useState(false)
-    const [isPriceVisible,setIsPriceVisible]=useState(false)
-    const [isRatingVisible,setIsRatingVisible]=useState(false);
-   
-    const handleClickBrand =()=>{
-setIsBrandsVisible(!isBrandsVisible)
-   }
+  const [isBrandsVisible, setIsBrandsVisible] = useState(false);
+  const [isPriceVisible, setIsPriceVisible] = useState(false);
+  const [isRatingVisible, setIsRatingVisible] = useState(false);
 
-   const  handlePriceRange=()=>{
-    setIsPriceVisible(!isPriceVisible)
-   }
+  const handleClickBrand = () => {
+    setIsBrandsVisible(!isBrandsVisible);
+  };
 
-   const handleRating=()=>{
-    setIsRatingVisible(!isRatingVisible)
-   }
+  const handlePriceRange = () => {
+    setIsPriceVisible(!isPriceVisible);
+  };
+
+  const handleRating = () => {
+    setIsRatingVisible(!isRatingVisible);
+  };
   return (
     <div className={styles.filterContainer}>
       <h1>Search Results</h1>
       <div>
-        <h4 className={styles.filterHeader} onClick={handleClickBrand}>Brand <img src={downArrow} alt="Brand" /></h4>
-        {isBrandsVisible?(<div className={styles.filterOption}>
-           <label> <input type="checkbox" name="Mango" id="" />Mango</label>
-           <label> <input type="checkbox" name="H&M" id="" />H&M</label>
-           <label> <input type="checkbox" name="Adidas" id="" />Adidas</label>
-           <label> <input type="checkbox" name="Allen Solly" id="" />Allen Solly</label>
-        </div>):null}
+        <h4
+          className={`${styles.filterHeader} ${
+            isBrandsVisible ? styles.open : null
+          }`}
+          onClick={handleClickBrand}
+        >
+          Brand <img src={downArrow} alt="Brand" />
+        </h4>
+        {isBrandsVisible ? (
+          <div className={styles.filterOption}>
+            <label>
+              {" "}
+              <input type="checkbox" name="Mango" id="" />
+              Mango
+            </label>
+            <label>
+              {" "}
+              <input type="checkbox" name="H&M" id="" />
+              H&M
+            </label>
+            <label>
+              {" "}
+              <input type="checkbox" name="Adidas" id="" />
+              Adidas
+            </label>
+            <label>
+              {" "}
+              <input type="checkbox" name="Allen Solly" id="" />
+              Allen Solly
+            </label>
+          </div>
+        ) : null}
       </div>
-      <hr/>
+      <hr />
       <div>
-        <h4 className={styles.filterHeader} onClick={handlePriceRange}>Price Range<img src={downArrow} alt="Price Range" /></h4>
-        {isPriceVisible?(<div className={styles.filterOption}>
-            <label> <input type="checkbox" name="under500" id="" />Under 500</label>
-            <label> <input type="checkbox" name="1000to3000" id="" />1000 To 3000</label>
-           
-        </div>):null}
+        <h4
+          className={`${styles.filterHeader} ${
+            isPriceVisible ? styles.open : null
+          }`}
+          onClick={handlePriceRange}
+        >
+          Price Range
+          <img src={downArrow} alt="Price Range" />
+        </h4>
+        {isPriceVisible ? (
+          <div className={styles.filterOption}>
+            <label>
+              {" "}
+              <input type="checkbox" name="under500" id="" />
+              Under 500
+            </label>
+            <label>
+              {" "}
+              <input type="checkbox" name="1000to3000" id="" />
+              1000 To 3000
+            </label>
+          </div>
+        ) : null}
       </div>
-      <hr/>
+      <hr />
       <div>
-        <h4 className={styles.filterHeader} onClick={handleRating}>Rating<img src={downArrow} alt="Rating" /></h4>
-        {isRatingVisible?(<div className={styles.filterOption}>
-            <label> <input type="checkbox" name="5star" id="" />Under 500</label>
-            <label> <input type="checkbox" name="4star" id="" />1000 To 3000</label>
-            <label> <input type="checkbox" name="3star" id="" />Under 500</label>
-            <label> <input type="checkbox" name="2star" id="" />Under 500</label>
-            <label> <input type="checkbox" name="1star" id="" />Under 500</label>
-           
-        </div>):null}
+        <h4
+          className={`${styles.filterHeader} ${
+            isRatingVisible ? styles.open : null
+          }`}
+          onClick={handleRating}
+        >
+          Rating
+          <img src={downArrow} alt="Rating" />
+        </h4>
+        {isRatingVisible ? (
+          <div className={styles.filterOption}>
+            <label>
+              {" "}
+              <input type="checkbox" name="5star" id="" />
+              Under 500
+            </label>
+            <label>
+              {" "}
+              <input type="checkbox" name="4star" id="" />
+              1000 To 3000
+            </label>
+            <label>
+              {" "}
+              <input type="checkbox" name="3star" id="" />
+              Under 500
+            </label>
+            <label>
+              {" "}
+              <input type="checkbox" name="2star" id="" />
+              Under 500
+            </label>
+            <label>
+              {" "}
+              <input type="checkbox" name="1star" id="" />
+              Under 500
+            </label>
+          </div>
+        ) : null}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Filter
+export default Filter;
