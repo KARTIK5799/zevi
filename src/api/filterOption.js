ec4b481e0200fd9c6342368f1f3f32456d5749f1
KARTIK5799 <thekartiksapkal@gmail.com>
2023-12-29 02:33:55 +0530

    accordian

--- src/api/filterOption.js
+++ src/api/filterOption.js
@@ -1,1 +1,5 @@
-const brand=["Mango", "H&M", "Adidas", "Allen Solly", "Nike", "Zara"];
+const brands=["Mango", "H&M", "Adidas", "Allen Solly", "Nike", "Zara"];
+const prices = ["Under 500", "1000 to 3000", "3000 to 5000", "Above 5000"];
+const ratings = ["5 Star", "4 Star", "3 Star", "2 Star", "1 Star"];
+
+export { brands, prices, ratings };
---
+++ src/components/Accordians/Accordian.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const Accordian = ({title,content}) => {
+    const [isOpen ,setIsOpen]=useState(false)
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Accordian
